5f9ec15f622ccb068d2f25022ae8dead0a416550
VladShag <126063712+VladShag@users.noreply.github.com>
2023-07-05 08:47:54 +0300

    ReadSettingsAdded

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,17 +1,37 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Client{
     public Socket socket;
-    private final int PORT = 8080;
-    private final String HOST = "localhost";
     public String name;
     public Client() {
+        ArrayList<String> settings = getSettings("src/main/resources/settings.txt");
+        int port = Integer.parseInt(settings.get(1));
+        String host = settings.get(0);
         try {
-            this.socket = new Socket(HOST, PORT);
+            this.socket = new Socket(host, port);
         } catch (IOException e) {}
             new ReadThread(this.socket, this).start();
             new WriteThread(this.socket, this).start();
+
+    }
+    public static ArrayList<String> getSettings(String filename) {
+        File file = new File(filename);
+        ArrayList<String> settings = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file);
+            String host = scanner.nextLine();
+            String[] hostSplited = host.split(":");
+            settings.add(hostSplited[1]);
+            String port = scanner.nextLine();
+            String[] portSplited = port.split(":");
+            settings.add(portSplited[1]);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return settings;
 
     }
 
